544b26deae0efee7de44cc304e258b850466cce1
EberteSampaio <ebertechavessampaiojr@gmail.com>
2025-01-05 12:12:24 -0300

    chore: incluindo annotation @Transactional e resolução de problemas com CORS

--- src/main/java/br/com/clients/crud_api/controller/ClientController.java
+++ src/main/java/br/com/clients/crud_api/controller/ClientController.java
@@ -5,11 +5,13 @@ import br.com.clients.crud_api.domain.dto.ClientResponse;
 import br.com.clients.crud_api.domain.service.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Stream;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/client")
 public class ClientController {
     @Autowired
@@ -23,26 +25,28 @@ public class ClientController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Transactional
     public ClientResponse create(@RequestBody ClientRequest clientRequest){
         return this.service.save(clientRequest);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ClientResponse edit( @PathVariable Long id, @RequestBody ClientRequest clientRequest){
+    @Transactional
+    public ClientResponse edit( @PathVariable("id") Long id, @RequestBody ClientRequest clientRequest){
         return this.service.edit(id, clientRequest);
     }
 
     @DeleteMapping("/{id}")
+    @Transactional
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable("id") Long id){
         this.service.delete(id);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.FOUND)
-    public ClientResponse findById(@PathVariable Long id){
+    public ClientResponse findById(@PathVariable("id") Long id){
         return this.service.findById(id);
     }
-
 }
